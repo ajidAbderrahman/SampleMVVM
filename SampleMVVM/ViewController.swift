0f4c26720dc41226d74e17d5a82bf72d808209d5
Abderrahman Ajid <abderrahman.ajid@gmail.com>
2022-05-24 07:26:55 +0100

    Initial Commit

---
+++ SampleMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleMVVM
+//
+//  Created by Abderrahman Ajid on 24/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
